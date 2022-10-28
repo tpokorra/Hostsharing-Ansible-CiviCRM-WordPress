eaea9accb43ebf69ab0934c64c8130dc05f566dc
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-10-28 16:23:55 +0200

    improve security of a CiviCRM installation
    allow access to the backend only for limited IP
    add a proxy for forms to be filled out by constituents

---
+++ templates/proxy.config.php
@@ -0,0 +1,8 @@
+<?php
+
+static::$AUTH_KEY = 'Bj5pnZEX6DkcG6Nz6AjDUT1bvcGRVhRaXDuKDX9CjsEs2';
+static::$ENABLE_AUTH = false;
+static::$DEBUG = false;
+static::$TARGET_URL = "https://{{domain}}";
+
+?>
